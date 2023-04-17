e1103a0cf5f9bee93dee601d19fe660f2b1da7d7
Arash Hosseini <arashhosseini733@gmail.com>
2023-04-17 20:05:42 +0330

    fix RunningProjectsList index style

--- app/components/layouts/main/RunningProjectsList.jsx
+++ app/components/layouts/main/RunningProjectsList.jsx
@@ -13,7 +13,7 @@ export default function RunningProjectsList() {
   const [selected, setSelected] = useState(people[0])
 
   return (
-    <div className='w-40 pb-2'>
+    <div className='w-40 pb-2 z-10'>
       <Listbox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
