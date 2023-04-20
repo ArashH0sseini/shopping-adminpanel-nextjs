8d976e1ee8a6acfb20939ee3d079032ba720e881
Arash Hosseini <arashhosseini733@gmail.com>
2023-04-20 18:36:47 +0330

    Implementation AccountMenu component

---
+++ app/components/layouts/navbar/AccountMenu.tsx
@@ -0,0 +1,74 @@
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { HiChevronDown } from "react-icons/hi";
+import Avatar from "../../../assets/images/avatar.png";
+import Image from "next/image";
+import { FaCrown } from "react-icons/fa";
+
+export default function AccountMenu() {
+  return (
+    <div>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="w-8 h-8 rounded-full overflow-hidden">
+            <Image
+              src={Avatar}
+              className="cursor-pointer"
+              alt="avatar"
+              objectFit="cover"
+            />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                <div className="flex space-x-2 cursor-pointer px-2 my-2 py-1 group w-full items-center text-orange-500 hover:bg-slate-100 text-sm font-bold">
+                  <FaCrown className="w-5 h-5" />
+                  <button>Go Pro</button>
+                </div>
+              </Menu.Item>
+            </div>
+            <div className="py-1">
+              <Menu.Item>
+                <button className="flex space-x-2 px-2 my-2 py-1 group w-full items-center text-gray-900 hover:bg-slate-100 text-sm font-semibold">
+                  Set status
+                </button>
+              </Menu.Item>
+              <Menu.Item>
+                <button className="flex space-x-2 px-2 my-2 py-1 group w-full items-center text-gray-900 hover:bg-slate-100 text-sm font-semibold">
+                  Profile & acccount
+                </button>
+              </Menu.Item>
+              <Menu.Item>
+                <button className="flex space-x-2 px-2 my-2 py-1 group w-full items-center text-gray-900 hover:bg-slate-100 text-sm font-semibold">
+                  Feedback
+                </button>
+              </Menu.Item>
+            </div>
+            <div className="py-1">
+              <Menu.Item>
+                <button className="flex space-x-2 px-2 my-2 py-1 group w-full items-center text-gray-900 hover:bg-slate-100 text-sm font-semibold">
+                  Settings
+                </button>
+              </Menu.Item>
+              <Menu.Item>
+                <button className="flex space-x-2 px-2 my-2 py-1 group w-full items-center text-gray-900 hover:bg-slate-100 text-sm font-semibold">
+                  Logout
+                </button>
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+}
--- app/components/layouts/navbar/RightSideNavbar.tsx
+++ app/components/layouts/navbar/RightSideNavbar.tsx
@@ -1,23 +1,22 @@
-import Avatar from "../../../assets/images/avatar.png";
 import { Tooltip as ReactTooltip } from "react-tooltip";
-import Image from "next/image";
 import { FaShoppingCart, FaMoon, FaSearch, FaSun } from "react-icons/fa";
 import { BsFillBellFill } from "react-icons/bs";
 import { useTheme } from "next-themes";
+import AccountMenu from "./AccountMenu";
 
 function RightSideNavbar() {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
-    <div className="flex items-center space-x-5">
+    <div className="flex items-center space-x-5 pr-2">
       <button
         onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
         className=""
       >
         {theme == "dark" ? (
           <FaSun className="w-8 h-8 text-yellow-400 bg-gray-800 hover:bg-gray-700 cursor-pointer p-2 rounded-full" />
-          ) : (
+        ) : (
           <FaMoon className="w-8 h-8 text-blue-600 bg-blue-100 hover:bg-slate-300 cursor-pointer p-2 rounded-full" />
         )}
       </button>
@@ -36,13 +35,8 @@ function RightSideNavbar() {
         <BsFillBellFill className="w-6 h-6 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 cursor-pointer" />
         <span className="absolute text-xs -top-1 -right-1 w-4 h-4 border-2 border-[#EDF2F9] bg-blue-500 text-white rounded-full flex items-center justify-center"></span>
       </div>
-      <div className="w-8 h-8 rounded-full overflow-hidden">
-        <Image
-          src={Avatar}
-          className="cursor-pointer"
-          alt="avatar"
-          objectFit="cover"
-        />
+      <div className="mt-2">
+        <AccountMenu />
       </div>
     </div>
   );
